0af9dff62f553e838c8a7308a1dc096601c603cb
ChrisEi <c.eitutis@gmail.com>
2022-04-15 13:22:09 -0500

    Express serves angular
    Set up the nodejs Express framework to send Angular. site is ready for first pass styling

---
+++ server.js
@@ -0,0 +1,28 @@
+// Use Express
+const express = require("express");
+const http = require('http');
+const path = require('path');
+
+
+// Create new instance of the express server
+const app = express();
+const port = process.env.PORT || 8080;
+
+
+// Create link to Angular build directory
+// The `ng build` command will save the result
+// under the `dist` folder.
+var distDir = __dirname + "/dist/frontend-portfolio";
+app.use(express.static(distDir));
+
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname))
+});
+
+
+// Init the server
+var server = app.listen(port, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
